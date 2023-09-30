762f07827bfa5794bb21e4bb921e0cecab0650d6
MovieRecomendation <chjayanth119@gmail.com>
2023-09-30 13:11:43 +0530

    staic files

--- iblogs/settings.py
+++ iblogs/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')],
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 MEDIA_URL = '/media/'
