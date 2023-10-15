87ef1e3c30b0756de1897cbcc5def45c8a5f8748
MovieRecomendation <chjayanth119@gmail.com>
2023-10-15 18:55:23 +0530

    style

--- blog/models.py
+++ blog/models.py
@@ -35,7 +35,9 @@ class Post(models.Model):
     content = models.TextField()
     url = models.CharField(max_length=100)
     cat = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
     image = models.ImageField(upload_to='post/')
+
     # likes = models.ManyToManyField(Ipmodel , related_name='post_likes' , blank=True)
     def __str__(self):
         return self.title
