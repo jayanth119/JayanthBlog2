aec1a5c5b3248874314a426c1489642faf30ad29
chukka jayanth <chjayanth119@gmail.com>
2024-04-09 23:03:32 +0530

    deployment changes
    add collective status

--- main_site/blog/urls.py
+++ main_site/blog/urls.py
@@ -1,12 +1,17 @@
-
 from django.urls import path
-# from django.conf.urls.static import static
-# from django.conf import settings
-from .views import home, post,category,main , about  
+from django.conf.urls.static import static
+from django.conf import settings
+from .views import home, post, category, main, about
+
 urlpatterns = [
-    path('', main, name= "main" ),
-    path('home/', home , name= "home" ),
-   path('blog/<slug:url>/', post, name='post') , 
-    path('category/<slug:url>',category), 
-    path('Jayanth' ,about , name="Jayanth") , 
+    path('', main, name="main"),
+    path('home/', home, name="home"),
+    path('blog/<slug:url>/', post, name='post'),
+    path('category/<slug:url>/', category, name='category'),
+    path('Jayanth/', about, name="Jayanth"),
 ]
+
+# Add static and media URL patterns only if DEBUG is True
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main_site/project/urls.py
+++ main_site/project/urls.py
@@ -1,8 +1,13 @@
-# urls.py
-
 from django.urls import path 
 from .views import all_projects
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', all_projects, name="all_projects"),
 ]
+
+# Add static and media URL patterns only if DEBUG is True
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
